470754dd8827bf4b52b9d296180278e2fb8b4d26
marcogbarcellos <marcogbarcellos@gmail.com>
2018-08-01 20:16:47 -0400

    refactor(config): use ENV variables on a config.js file

--- config/config.js
+++ config/config.js
@@ -1,4 +1,11 @@
 module.exports = {
     port: process.env.PORT || 3060,
     host: process.env.HOST || '0.0.0.0',
+    database: {
+        database: process.env.DB_NAME || "opencollective_ledger_dvl",
+        username: process.env.DB_USER || "opencollective",
+        password: process.env.DB_PASSWORD || "",
+        host: process.env.DB_HOST || "127.0.0.1",
+        dialect: process.env.DB_DIALECT || "postgres",
+    },
 };
