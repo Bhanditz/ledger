7874707d24035c54311001cc0dde1a954969a85d
marcogbarcellos <marcogbarcellos@gmail.com>
2018-08-10 21:42:34 -0400

    fix(models): index was looking for folders instead of only files

--- server/models/index.js
+++ server/models/index.js
@@ -5,14 +5,14 @@ import Sequelize from 'sequelize';
 import config from '../../config/config';
 
 
-const sequelize = new Sequelize(config.database.database, config.database.username, config.database.password, {
+export const sequelize = new Sequelize(config.database.database, config.database.username, config.database.password, {
   dialect: config.database.dialect,
   logging: false,
 });
 
 // Load all models present in the model directory
 const models = Object.assign({}, ...fs.readdirSync(__dirname)
-  .filter(file => (file.indexOf('.') !== 0) && (file !== 'index.js'))
+  .filter(file => (file.indexOf('.js') > -1) && (file !== 'index.js'))
   .map((file) => {
     const model = require(path.join(__dirname, file));
     return { [model.name]: model.init(sequelize) };
