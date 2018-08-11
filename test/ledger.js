c30cac5e627373c8aa03b4c035dcdfcdd408da4c
marcogbarcellos <marcogbarcellos@gmail.com>
2018-08-10 21:41:19 -0400

    feat(docs): adding documentation api and script, add draft to create tests

---
+++ test/ledger.js
@@ -0,0 +1,22 @@
+// import { expect } from 'chai';
+// import sinon from 'sinon';
+// import models from '../server/models';
+
+describe('ledger', () => {
+
+
+  // 2. add accounts
+  // 3. add wallets to accounts
+  // 4. user 1 "cash in" 30usd
+  // 5. then 2 transactions should show up
+  //        - 1: type: DEBIT, ToAccountId: NULL, FromAccountId: User1, Amount: -30, currency:USD, toWalletId: User1_CreditCard, fromWalletId: User1_USD
+  //        - 2: type: CREDIT, ToAccountId: User1, FromAccountId: NULL, Amount: 30,  currency:USD, toWalletId: User1_USD , fromWalletId: User1_CreditCard
+  // 6. user1 with balance send money to user 2 in the same currency
+  // 7. user1 send MultiCurrency transaction(from USD to EUR for example) to user2
+  // 8. user1 "cashes" out
+  // 1. combination of numbers 4, 5, 6, 7 and 8 to check whether ledger balance is 0
+  // 1. combination of numbers 4, 5, 6, 7 and 8 to check How much money we have in the System
+  // 1. combination of numbers 4, 5, 6, 7 and 8 to check the total of money cashed in for a User
+  // 1. combination of numbers 4, 5, 6, 7 and 8 to check the total of money cashed out for a User
+
+});
