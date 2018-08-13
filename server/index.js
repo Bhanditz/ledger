d1f6628a200c4555793fcce72c73ec18b2166aab
marcogbarcellos <marcogbarcellos@gmail.com>
2018-08-13 17:18:47 -0400

    fix(index): removing console log to use logger.info

--- server/index.js
+++ server/index.js
@@ -4,7 +4,9 @@ import os from 'os';
 import config from '../config/config';
 import models from './models';
 import pino from 'express-pino-logger';
+import Logger from './globals/logger';
 
+const logger  = new Logger();
 const app = express();
 app.use(express.json());
 app.use(pino());
@@ -15,7 +17,7 @@ app.models = models;
  */
 const server = app.listen(config.port, config.host, () => {
   const host = os.hostname();
-  console.log('Open Collective API listening at http://%s:%s in %s environment.\n', host, server.address().port, app.set('env'));
+  logger.info(`Open Collective API listening at http://${host}:${server.address().port} in ${app.set('env')} environment.\n`);
 });
 
 export default app;
