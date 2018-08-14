618ddf36b2c1bae381b7344db4551588f6f52c6e
marcogbarcellos <marcogbarcellos@gmail.com>
2018-08-14 15:57:48 -0400

    refactor(services/transaction): rebuild logic of transactionService according to new strategy and utils

--- server/services/transactionService.js
+++ server/services/transactionService.js
@@ -1,8 +1,6 @@
 import models, { sequelize } from '../models';
 import AbstractCrudService from './abstractCrudService';
-import { transactionTypeEnum } from '../globals/enums/transactionTypeEnum';
-import TransactionCashin from '../lib/strategies/transactions/transactionCashin';
-import TransactionCashout from '../lib/strategies/transactions/transactionCashout';
+import TransactionCashFlow from '../lib/strategies/transactions/transactionCashFlow';
 import TransactionAccountToAccountFx from '../lib/strategies/transactions/transactionAccountToAccountFx';
 import TransactionAccountToAccount from '../lib/strategies/transactions/transactionAccountToAccount';
 import Logger from '../globals/logger';
@@ -14,10 +12,16 @@ export default class TransactionService extends AbstractCrudService {
     this.logger = new Logger();
   }
 
-  create(data) {
-    const strategy = this._defineTransactionStrategy(data);
+  /** Given a transaction, identify which kind of transaction it will be and
+  **  persists the group of transactions it will generate
+  * @param {Object} incomingTransaction - transaction
+  * @return {Array} containing the original incoming transaction + its double entry equivalent.
+  */
+  async create(data) {
     // the strategy will return an array of transactions already formatted for the db
-    const transactions = strategy.getTransactions();
+    const strategy = this._defineTransactionStrategy(data);
+    const transactions = await strategy.getTransactions();
+
     // Creating a Sequelize "Managed transaction" which automatically commits
     // if all transactions are done or rollback if any of them fail.
     return sequelize.transaction( t => {
@@ -31,57 +35,21 @@ export default class TransactionService extends AbstractCrudService {
     });
   }
 
+
+  /** Given a transaction, return the related "strategy" Object
+  * @param {Object} incomingTransaction - transaction
+  * @return {Object} strategy - Return defined Strategy Class Object
+  */
   _defineTransactionStrategy(transaction) {
     // Cashin Or Cashout have the same Account(From and To)
     if (transaction.FromAccountId === transaction.ToAccountId) {
-      if (transaction.amount > 0) {
-        return new TransactionCashin(transaction);
-      }
-      return new TransactionCashout(transaction);
+      return new TransactionCashFlow(transaction);
     }
     // If both toAmount and toCurrency are defined, it means it's an FX Transaction
     if (transaction.toAmount && transaction.toCurrency) {
       return new TransactionAccountToAccountFx(transaction);
     }
+    // Defaults to an Account to Account Transaction
     return new TransactionAccountToAccount(transaction);
   }
-
-  /** Given a transaction, return the same transaction plus its
-  **  double entry equivalent transaction
-  * @param {Object} incomingTransaction - transaction
-  * @return {Array} containing the original incoming transaction + its double entry equivalent.
-  */
-  generateDoubleEntryTransactions(incomingTransaction) {
-    const doubleEntryTransactionType = (incomingTransaction.type === transactionTypeEnum.DEBIT)
-      ? transactionTypeEnum.CREDIT
-      : transactionTypeEnum.DEBIT;
-
-    return [
-      incomingTransaction,
-      {
-        FromAccountId: incomingTransaction.ToAccountId ? incomingTransaction.ToAccountId : null,
-        ToAccountId: incomingTransaction.FromAccountId ? incomingTransaction.FromAccountId : null,
-        FromWalletId: incomingTransaction.ToWalletId,
-        ToWalletId: incomingTransaction.FromWalletId,
-        type: doubleEntryTransactionType,
-        TransactionGroup: incomingTransaction.TransactionGroup,
-        currency: incomingTransaction.currency,
-        amount: (-1 * incomingTransaction.amount),
-        transactionGroupSequence: incomingTransaction.transactionGroupSequence + 1,
-      },
-    ];
-  }
-
-  generateCashoutTransaction(incomingTransaction) {
-    this._checkAndInsertTransactionGroup(incomingTransaction);
-    const cashoutTransactions = this.generateDoubleEntryTransactions(incomingTransaction);
-    return cashoutTransactions;
-  }
-
-  generateCashinTransaction(incomingTransaction) {
-    this._checkAndInsertTransactionGroup(incomingTransaction);
-    const cashoutTransactions = this.generateDoubleEntryTransactions(incomingTransaction);
-    return cashoutTransactions;
-  }
-
 }
