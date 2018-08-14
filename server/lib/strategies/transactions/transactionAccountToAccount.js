e11fe874ee82f175e99e96f915a1f8b3781372ec
marcogbarcellos <marcogbarcellos@gmail.com>
2018-08-14 16:01:37 -0400

    feat(strategy/transaction/accountToAccount): build logic using transactionUtil lib functions

--- server/lib/strategies/transactions/transactionAccountToAccount.js
+++ server/lib/strategies/transactions/transactionAccountToAccount.js
@@ -1,12 +1,42 @@
 import AbstractTransactionStrategy from './abstractTransactionStrategy';
+import WalletUtil from '../../walletUtil';
+import TransactionUtil from '../../transactionUtil';
+import TransactionCashFlow from './transactionCashFlow';
 
 export default class TransactionAccountToAccount extends AbstractTransactionStrategy {
 
   constructor(incomingTransaction) {
     super(incomingTransaction);
+    this.walletUtil = new WalletUtil();
+    this.transactionUtil = new TransactionUtil();
   }
 
-  getTransactions (){
+  async getTransactions (){
+    let transactions = [];
+    const fromWalletBalance = await this.walletUtil.getCurrencyBalanceFromWalletId(this.incomingTransaction.currency, this.incomingTransaction.FromWalletId);
+    // If there is no money in Account Wallet, then it needs to Cash In
+    if ( fromWalletBalance < this.incomingTransaction.amount) {
+      const cashInTransaction = {
+        FromAccountId: this.incomingTransaction.FromAccountId,
+        ToAccountId: this.incomingTransaction.FromAccountId,
+        FromWalletId: null, // Setting up as null will make it look for the default Cash In Wallet 
+        ToWalletId: this.incomingTransaction.FromWalletId,
+        amount: this.incomingTransaction.amount - fromWalletBalance, // topup the remaning amount to complete tx
+        transactionGroupId: this.incomingTransaction.transactionGroupId,
+        transactionGroupSequence: this.incomingTransaction.transactionGroupSequence,
+      };
+      const transactionCashFlow = new TransactionCashFlow(cashInTransaction);
+      const cashInTransactions = await transactionCashFlow.getTransactions();
+      transactions = transactions.concat(cashInTransactions);
+      // after updating the transactions array, we need to get the last
+      // transactionGroupSequence and iterate so we would know the next
+      // transaction from the sequence
+      this.incomingTransaction.transactionGroupSequence =
+        transactions[transactions.length-1].transactionGroupSequence + 1;
+    }
+    const account2AccountTransactions = this.transactionUtil.getDoubleEntryArray(this.incomingTransaction);
+    transactions = transactions.concat(account2AccountTransactions);
+    return transactions;
   }
 
 }
