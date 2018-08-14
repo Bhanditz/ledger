5b3a7b9a598702c32bbbfc4d696f345179cab247
marcogbarcellos <marcogbarcellos@gmail.com>
2018-08-14 15:53:03 -0400

    feat(routes/account): adding Account Put endpoint

--- server/routes/accountRouter.js
+++ server/routes/accountRouter.js
@@ -1,3 +1,4 @@
+import HttpStatus from 'http-status-codes';
 import AbstractRouter from './abstractRouter';
 import AccountService from '../services/accountService';
 import AccountValidator from './validators/accountValidator';
@@ -6,5 +7,19 @@ export default class AccountRouter extends AbstractRouter {
 
   constructor(app) {
     super(app, new AccountService(), 'accounts', new AccountValidator());
+  }
+
+  put() {
+    this.app.put(this.path, (req, res) => {
+      if (!req.query.id) {
+        throw Error('Account id undefined');
+      }
+      this.service.put(req.body, { id: req.query.id } ).then( data => {
+        res.send({ updated: data });
+      }).catch(error => {
+        this.logger.error(error);
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(error);
+      });
+    });
   }
 }
