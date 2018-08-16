467b9af318532b31fa02fc7b203869ac8c88ab46
marcogbarcellos <marcogbarcellos@gmail.com>
2018-08-15 21:59:38 -0400

    feat(server): add dotenv config files and load them

--- server/index.js
+++ server/index.js
@@ -4,7 +4,14 @@ import os from 'os';
 import config from '../config/config';
 import models from './models';
 import pino from 'express-pino-logger';
+import path from 'path';
 import Logger from './globals/logger';
+
+// Default environment is development to prevent "accidents"
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+//loading ENV vars
+require('dotenv').config({ path: path.resolve(__dirname, `../${process.env.NODE_ENV}.env`) });
 
 const logger  = new Logger();
 const app = express();
