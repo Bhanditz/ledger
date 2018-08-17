975b1c99c00d8d9b03c3b5fdb2f1d1ef811c7bcb
marcogbarcellos <marcogbarcellos@gmail.com>
2018-08-17 13:48:04 -0400

    feat(scripts): add scripts to create and migrate test db

---
+++ scripts/recreate_test_db_and_migrate.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# defining API PORT
+PORT=3062
+# defining DB_NAME
+DB_NAME=opencollective_ledger_test
+
+# recreating db
+dropdb -U postgres $DB_NAME
+createdb -U postgres $DB_NAME
+
+# creating db schema
+NODE_ENV=test npm run db:migrate
