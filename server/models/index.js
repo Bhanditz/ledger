282c06ae92c177c7de37c8441779edb41e856710
marcogbarcellos <marcogbarcellos@gmail.com>
2018-08-17 13:52:49 -0400

    fix(models): importing sequelize and models inside Database class

--- server/models/index.js
+++ server/models/index.js
@@ -3,26 +3,25 @@ import fs from 'fs';
 import path from 'path';
 import Sequelize from 'sequelize';
 import config from '../../config/config';
-export let sequelize = null, models = null;
 
 export default class Database {
   constructor() {
-    if (!sequelize) {
-      sequelize = new Sequelize(config.database.database, config.database.username, config.database.password, {
+    if (!this.sequelize) {
+      this.sequelize = new Sequelize(config.database.database, config.database.username, config.database.password, {
         dialect: config.database.dialect,
         logging: false,
       });
     }
-    if (!models) {
-      models = Object.assign({}, ...fs.readdirSync(__dirname)
+    if (!this.models) {
+      this.models = Object.assign({}, ...fs.readdirSync(__dirname)
         .filter(file => (file.indexOf('.js') > -1) && (file !== 'index.js'))
         .map((file) => {
           const model = require(path.join(__dirname, file));
-          return { [model.name]: model.init(sequelize) };
+          return { [model.name]: model.init(this.sequelize) };
         })
       );
-      for (const model of Object.keys(models)) {
-        typeof models[model].associate === 'function' && models[model].associate(models);
+      for (const model of Object.keys(this.models)) {
+        typeof this.models[model].associate === 'function' && this.models[model].associate(this.models);
       }
     }
   }
