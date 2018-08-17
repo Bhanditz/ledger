e6ba72bbe6db4ebeb68710def2c3eaf0f45fa398
marcogbarcellos <marcogbarcellos@gmail.com>
2018-08-17 15:52:31 -0400

    feat(docs): adding endpoints to rest api documentation

--- server/routes/accountRouter.js
+++ server/routes/accountRouter.js
@@ -9,6 +9,40 @@ export default class AccountRouter extends AbstractRouter {
     super(app, new AccountService(), 'accounts', new AccountValidator());
   }
 
+  /**
+   * @api {get} /accounts Get Accounts
+   * @apiName get_accounts
+   * @apiGroup Accounts
+   *
+   * @apiParam {String} [slug] currency of the amount field
+   * @apiParam {Number} [DefaultCashinWalletId] id of the default cashin wallet
+   * @apiParam {Number} [DefaultCashoutWalletId] id of the default cashout wallet
+   * @apiParam {String} [createdAt] Date the account was created
+   * @apiParam {String} [updatedAt] Date the account was updated
+   */
+  get() {
+    super.get();
+  }
+
+  /**
+   * @api {post} /accounts Post Accounts
+   * @apiName post_accounts
+   * @apiGroup Accounts
+   *
+   * @apiParam {String} slug currency of the amount field
+   */
+  post() {
+    super.post();
+  }
+
+  /**
+   * @api {put} /accounts Put Accounts
+   * @apiName put_accounts
+   * @apiGroup Accounts
+   * @apiParam {Number} [slug] unique string identifier of the account
+   * @apiParam {Number} [DefaultCashinWalletId] id of the default cashin wallet
+   * @apiParam {Number} [DefaultCashoutWalletId] id of the default cashout wallet
+   */
   put() {
     this.app.put(this.path, (req, res) => {
       if (!req.query.id) {
