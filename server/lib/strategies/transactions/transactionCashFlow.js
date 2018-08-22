f189ed430d7d8d0aaf06fbcf6d3fe65f07f2be1c
marcogbarcellos <marcogbarcellos@gmail.com>
2018-08-21 22:05:10 -0400

    fix(strategies/transactioncashflow): remove defaultcash wallet from strategy

--- server/lib/strategies/transactions/transactionCashFlow.js
+++ server/lib/strategies/transactions/transactionCashFlow.js
@@ -11,17 +11,7 @@ export default class TransactionCashFlow extends AbstractTransactionStrategy {
   }
 
   async getTransactions() {
-    // setting toAccountId to Null(as both from and To are the same, we need only one to set the other to null)
-    const cashFlowTransaction = this.incomingTransaction;
-    // If there is NO FromWalletId defined it defaults to Cashin Wallet
-    if (!this.incomingTransaction.FromWalletId) {
-      this.incomingTransaction.FromWalletId = await this.accountUtil.getDefaultCashinWalletId(this.incomingTransaction.FromAccountId);
-    }
-    // If there is NO ToWalletId defined it defaults to Cashout Wallet
-    if (!this.incomingTransaction.ToWalletId) {
-      this.incomingTransaction.ToWalletId = await this.accountUtil.getDefaultCashoutWalletId(this.incomingTransaction.FromAccountId);
-    }
-    return this.transactionUtil.getDoubleEntryArray(cashFlowTransaction);
+    return this.transactionUtil.getDoubleEntryArray(this.incomingTransaction);
   }
 
 }
