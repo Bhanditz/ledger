9b3ff74f7baa9a4e2059ad4fa840127591dd087b
marcogbarcellos <marcogbarcellos@gmail.com>
2018-08-22 21:17:50 -0400

    refactor(strategies/transactionCashFlow): instead of setting default cash ins and outs wallets, automatically cash in ONLY if payment method is service stripe and type creditcard, otherwise throw error

--- server/lib/strategies/transactions/transactionCashFlow.js
+++ server/lib/strategies/transactions/transactionCashFlow.js
@@ -1,16 +1,26 @@
 import AbstractTransactionStrategy from './abstractTransactionStrategy';
 import TransactionUtil from '../../transactionUtil';
-import AccountUtil from '../../accountUtil';
+import WalletUtil from '../../walletUtil';
+import { operationNotAllowed } from '../../../globals/errors';
+import Wallet from '../../../models/Wallet';
+import { paymentMethodServices } from '../../../globals/enums/paymentMethodServices';
 
 export default class TransactionCashFlow extends AbstractTransactionStrategy {
 
   constructor(incomingTransaction) {
     super(incomingTransaction);
-    this.accountUtil = new AccountUtil();
     this.transactionUtil = new TransactionUtil();
+    this.walletUtil = new WalletUtil();
   }
 
   async getTransactions() {
+    const fromWallet = await Wallet.findById(this.incomingTransaction.FromWalletId);
+    const fromWalletBalance = await this.walletUtil.getCurrencyBalanceFromWalletId(this.incomingTransaction.currency, this.incomingTransaction.FromWalletId);
+    // If there is no money in Account Wallet, then it needs to Cash In
+    if ( fromWalletBalance < this.incomingTransaction.amount &&
+         fromWallet.paymentMethodType != paymentMethodServices.stripe.types.CREDITCARD) {
+      throw Error(operationNotAllowed(`Wallet(id ${this.incomingTransaction.FromWalletId}) does not have enough balance to complete transaction`));
+    }
     return this.transactionUtil.getDoubleEntryArray(this.incomingTransaction);
   }
 
