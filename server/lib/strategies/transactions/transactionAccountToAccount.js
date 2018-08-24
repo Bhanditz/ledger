5dbb8bc521b91e2846b5b45ed1fb795e91d72522
marcogbarcellos <marcogbarcellos@gmail.com>
2018-08-23 20:22:08 -0400

    fix(lib/strategies): fixing naming issue

--- server/lib/strategies/transactions/transactionAccountToAccount.js
+++ server/lib/strategies/transactions/transactionAccountToAccount.js
@@ -1,27 +1,26 @@
 import AbstractTransactionStrategy from './abstractTransactionStrategy';
-import WalletUtil from '../../walletUtil';
-import TransactionUtil from '../../transactionUtil';
-import TransactionCashFlow from './transactionCashFlow';
+import WalletLib from '../../walletLib';
+import TransactionLib from '../../transactionLib';
 import { operationNotAllowed } from '../../../globals/errors';
 
 export default class TransactionAccountToAccount extends AbstractTransactionStrategy {
 
   constructor(incomingTransaction) {
-    const transactionUtil = new TransactionUtil();
-    transactionUtil.validateTransaction(incomingTransaction);
+    const transactionLib = new TransactionLib();
+    transactionLib.validateTransaction(incomingTransaction);
     super(incomingTransaction);
-    this.walletUtil = new WalletUtil();
-    this.transactionUtil = transactionUtil;
+    this.walletLib = new WalletLib();
+    this.transactionLib = transactionLib;
   }
 
   async getTransactions (){
     let transactions = [];
-    const fromWalletBalance = await this.walletUtil.getCurrencyBalanceFromWalletId(this.incomingTransaction.currency, this.incomingTransaction.FromWalletId);
+    const fromWalletBalance = await this.walletLib.getCurrencyBalanceFromWalletId(this.incomingTransaction.currency, this.incomingTransaction.FromWalletId);
     // Do not allowing creating transactions If there is no money in Account Wallet
     if ( fromWalletBalance < this.incomingTransaction.amount) {
       throw Error(operationNotAllowed(`Wallet(id ${this.incomingTransaction.FromWalletId}) does not have enough balance to complete transaction`));
     }
-    const account2AccountTransactions = this.transactionUtil.getDoubleEntryArray(this.incomingTransaction);
+    const account2AccountTransactions = this.transactionLib.getDoubleEntryArray(this.incomingTransaction);
     transactions = transactions.concat(account2AccountTransactions);
     return transactions;
   }
--- server/lib/strategies/transactions/transactionCashFlowForex.js
+++ server/lib/strategies/transactions/transactionCashFlowForex.js
@@ -1,6 +1,6 @@
 import AbstractTransactionStrategy from './abstractTransactionStrategy';
-import TransactionUtil from '../../transactionUtil';
-import WalletUtil from '../../walletUtil';
+import TransactionLib from '../../transactionLib';
+import WalletLib from '../../walletLib';
 import { operationNotAllowed } from '../../../globals/errors';
 import Wallet from '../../../models/Wallet';
 import { paymentMethodServices } from '../../../globals/enums/paymentMethodServices';
@@ -9,19 +9,19 @@ export default class TransactionCashFlowForex extends AbstractTransactionStrateg
 
   constructor(incomingTransaction) {
     super(incomingTransaction);
-    this.transactionUtil = new TransactionUtil();
-    this.walletUtil = new WalletUtil();
+    this.transactionLib = new TransactionLib();
+    this.walletLib = new WalletLib();
   }
 
   async getTransactions() {
     const fromWallet = await Wallet.findById(this.incomingTransaction.FromWalletId);
-    const fromWalletBalance = await this.walletUtil.getCurrencyBalanceFromWalletId(this.incomingTransaction.currency, this.incomingTransaction.FromWalletId);
+    const fromWalletBalance = await this.walletLib.getCurrencyBalanceFromWalletId(this.incomingTransaction.currency, this.incomingTransaction.FromWalletId);
     // If there is no money in Account Wallet, then it needs to Cash In
     if ( fromWalletBalance < this.incomingTransaction.amount &&
-         fromWallet.paymentMethodType != paymentMethodServices.stripe.types.CREDITCARD) {
+         fromWallet.type != paymentMethodServices.stripe.types.CREDITCARD) {
       throw Error(operationNotAllowed(`Wallet(id ${this.incomingTransaction.FromWalletId}) does not have enough balance to complete transaction`));
     }
-    return this.transactionUtil.getDoubleEntryArray(this.incomingTransaction);
+    return this.transactionLib.getDoubleEntryArray(this.incomingTransaction);
   }
 
 }
