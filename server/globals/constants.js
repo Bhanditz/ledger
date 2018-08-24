f22f09ee3cee5333517cfb6381328da380c1036f
marcogbarcellos <marcogbarcellos@gmail.com>
2018-08-23 20:17:48 -0400

    feat(globals/constants): adding Stripe and Paypal constants(fake for now)

--- server/globals/constants.js
+++ server/globals/constants.js
@@ -1,4 +1,8 @@
 export const constants = Object.freeze({
   PLATFORM_ACCOUNT_ID: 10,
   PLATFORM_WALLET_ID: 10,
+  STRIPE_ACCOUNT_ID: 11,
+  STRIPE_WALLET_ID: 11,
+  PAYPAL_ACCOUNT_ID: 12,
+  PAYPAL_WALLET_ID: 12,
 });
