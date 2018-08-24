d909986908014dc502c697714fb55cee24b22662
marcogbarcellos <marcogbarcellos@gmail.com>
2018-08-23 20:23:55 -0400

    feat(lib/strategies/transactionCashFlow): adding Fee Transactions if FromWallet has Service Stripe and Type CreditCard

--- server/lib/strategies/transactions/transactionCashFlow.js
+++ server/lib/strategies/transactions/transactionCashFlow.js
@@ -1,27 +1,49 @@
 import AbstractTransactionStrategy from './abstractTransactionStrategy';
-import TransactionUtil from '../../transactionUtil';
-import WalletUtil from '../../walletUtil';
+import TransactionLib from '../../transactionLib';
+import WalletLib from '../../walletLib';
 import { operationNotAllowed } from '../../../globals/errors';
 import Wallet from '../../../models/Wallet';
 import { paymentMethodServices } from '../../../globals/enums/paymentMethodServices';
+import PaymentProviderFeeTransactions from '../../feeTransactions/paymentProviderFeeTransactions';
+import PlatformFeeTransactions from '../../feeTransactions/platformFeeTransactions';
 
 export default class TransactionCashFlow extends AbstractTransactionStrategy {
 
   constructor(incomingTransaction) {
     super(incomingTransaction);
-    this.transactionUtil = new TransactionUtil();
-    this.walletUtil = new WalletUtil();
+    this.transactionLib = new TransactionLib();
+    this.walletLib = new WalletLib();
   }
 
   async getTransactions() {
+    let transactions = [];
     const fromWallet = await Wallet.findById(this.incomingTransaction.FromWalletId);
-    const fromWalletBalance = await this.walletUtil.getCurrencyBalanceFromWalletId(this.incomingTransaction.currency, this.incomingTransaction.FromWalletId);
+    const fromWalletBalance = await this.walletLib.getCurrencyBalanceFromWalletId(this.incomingTransaction.currency, this.incomingTransaction.FromWalletId);
     // If there is no money in Account Wallet, then it needs to Cash In
     if ( fromWalletBalance < this.incomingTransaction.amount &&
-         fromWallet.paymentMethodType != paymentMethodServices.stripe.types.CREDITCARD) {
+         fromWallet.type !== paymentMethodServices.stripe.types.CREDITCARD) {
       throw Error(operationNotAllowed(`Wallet(id ${this.incomingTransaction.FromWalletId}) does not have enough balance to complete transaction`));
     }
-    return this.transactionUtil.getDoubleEntryArray(this.incomingTransaction);
+    // It means it's money coming into the system, so some fees apply
+    if ( fromWallet.type === paymentMethodServices.stripe.types.CREDITCARD ) {
+      // Adding Platform Fees Transactions
+      const platformFeeTransaction = new PlatformFeeTransactions(this.incomingTransaction);
+      await platformFeeTransaction.setTransactionInfo();
+      transactions = transactions.concat(platformFeeTransaction.getFeeDoubleEntryTransactions());
+      // incrementing transaction groupSequence according to the last transaction in the array
+      this.incomingTransaction.transactionGroupSequence =
+        transactions[transactions.length - 1].transactionGroupSequence + 1;
+
+       // Adding Payment Providers Fees Transactions
+      const paymentProviderFeeTransactions = new PaymentProviderFeeTransactions(this.incomingTransaction);
+      await paymentProviderFeeTransactions.setTransactionInfo();
+      transactions = transactions.concat(paymentProviderFeeTransactions.getFeeDoubleEntryTransactions());
+      // once more incrementing transaction groupSequence according to the last transaction in the array
+      this.incomingTransaction.transactionGroupSequence =
+        transactions[transactions.length - 1].transactionGroupSequence + 1;
+    }
+    transactions = transactions.concat(this.transactionLib.getDoubleEntryArray(this.incomingTransaction));
+    return transactions;
   }
 
 }
