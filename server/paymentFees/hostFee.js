0bf8e55c99a4cc6562bd46967bdab4962d021cee
marcogbarcellos <marcogbarcellos@gmail.com>
2018-09-03 22:18:21 -0400

    fix(paymentFees): removing paymentfees transactions generator

--- server/paymentFees/hostFee.js
+++
@@ -1,9 +0,0 @@
-import AbstractFee from './abstractFee';
-
-export default class HostFee extends AbstractFee {
-
-  constructor(transaction, fixedFee, percentFee, feeAccountId, feeWalletId){
-    super(transaction, fixedFee, percentFee, feeAccountId, feeWalletId);
-  }
-
-}
