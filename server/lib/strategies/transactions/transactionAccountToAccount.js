cc9d2182cb06d907d064f2d2858c945eeeba8836
marcogbarcellos <marcogbarcellos@gmail.com>
2018-09-04 17:02:00 -0400

    refactor(lib/strategies/transactionAccounttoAccount): tweak on getTransactions method

--- server/lib/strategies/transactions/transactionAccountToAccount.js
+++ server/lib/strategies/transactions/transactionAccountToAccount.js
@@ -14,15 +14,12 @@ export default class TransactionAccountToAccount extends AbstractTransactionStra
   }
 
   async getTransactions (){
-    let transactions = [];
     const fromWalletBalance = await this.walletLib.getCurrencyBalanceFromWalletId(this.incomingTransaction.currency, this.incomingTransaction.FromWalletId);
     // Do not allowing creating transactions If there is no money in Account Wallet
     if ( fromWalletBalance < this.incomingTransaction.amount) {
       throw Error(operationNotAllowed(`Wallet(id ${this.incomingTransaction.FromWalletId}) does not have enough balance to complete transaction`));
     }
-    const account2AccountTransactions = this.transactionLib.getDoubleEntryArray(this.incomingTransaction);
-    transactions = transactions.concat(account2AccountTransactions);
-    return transactions;
+    return this.transactionLib.getDoubleEntryArray(this.incomingTransaction);
   }
 
 }
