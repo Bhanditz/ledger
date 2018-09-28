1dad9af2602b1f2a9729981fbf8f9481bde27baa
marcogbarcellos <marcogbarcellos@gmail.com>
2018-09-27 21:09:36 -0400

    feat: start testing migration from current prod to ledger

---
+++ scripts/old-production-migration/transactionsTableMigration.js
@@ -0,0 +1,16 @@
+import { Client } from 'pg';
+
+const client = new Client({
+  user: 'marco',
+  host: 'localhost',
+  database: 'opencollective_prod_snapshot',
+  // password: 'secretpassword',
+  port: 5432,
+});
+
+client.connect();
+
+client.query('SELECT * FROM "Collectives" limit 10', (err, res) => {
+  console.log(err, res);
+  client.end();
+})
