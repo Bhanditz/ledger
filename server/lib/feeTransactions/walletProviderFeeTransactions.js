e53fee9f2a7b79690c78c335c91f58ae5f14de09
marcogbarcellos <marcogbarcellos@gmail.com>
2018-10-04 21:04:36 -0400

    fix(feeTransactions/walletProvider): remove instatiated Lib added to 'this' that made it circular and adding category field

--- server/lib/feeTransactions/walletProviderFeeTransactions.js
+++ server/lib/feeTransactions/walletProviderFeeTransactions.js
@@ -1,23 +1,25 @@
 import AbstractFeeTransactions from './abstractFeeTransactions';
 import ProviderLib from '../providerLib';
+import { transactionCategoryEnum } from '../../globals/enums/transactionCategoryEnum';
 
 export default class WalletProviderFeeTransactions extends AbstractFeeTransactions {
 
   constructor(transaction) {
     super(transaction);
-    this.providerLib = new ProviderLib();
   }
 
   async setTransactionInfo() {
     try {
+      const providerLib = new ProviderLib();
       this.feeAccountId = this.transaction.fromWalletProvider.OwnerAccountId;
-      const providerCurrencyWallet = await this.providerLib.findOrCreateWalletByCurrency(this.transaction.fromWalletProvider, this.transaction.currency);
+      const providerCurrencyWallet = await providerLib.findOrCreateWalletByCurrency(this.transaction.fromWalletProvider, this.transaction.currency);
       this.feeWalletId = providerCurrencyWallet.id;
       const fixedFee = this.transaction.fromWalletProvider.fixedFee ? this.transaction.fromWalletProvider.fixedFee : 0;
       const percentFee = this.transaction.fromWalletProvider.percentFee ? this.transaction.fromWalletProvider.percentFee : 0;
       // check if it's a forex or regular transaction
       const amount = this.transaction.destinationAmount ||  this.transaction.amount;
       this.fee = fixedFee + (amount * percentFee);
+      this.category = transactionCategoryEnum.WALLET_PROVIDER;
     } catch (error) {
       throw error;
     }
