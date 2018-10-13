8cffbd3dda9814946476782488475f8d08ed8835
marcogbarcellos <marcogbarcellos@gmail.com>
2018-10-12 22:48:57 -0400

    feat(script/migration): adding mapping to identify fields in current prod ledger to the new ledger

---
+++ scripts/old-production-migration/mapping.js
@@ -0,0 +1,8 @@
+
+export const mapping = Object.freeze({
+    platformFee: 'platformFeeInHostCurrency',
+    walletProviderFee: 'hostFeeInHostCurrency',
+    paymentProviderFee: 'paymentProcessorFeeInHostCurrency',
+    destinationAmount: 'amountInHostCurrency',
+    destinationCurrency: 'hostCurrency',
+});
