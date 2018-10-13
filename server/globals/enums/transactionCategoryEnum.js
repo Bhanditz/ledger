397c5fba9ec48d497982be35b9a3499dab73baf5
marcogbarcellos <marcogbarcellos@gmail.com>
2018-10-12 22:49:21 -0400

    style: indentation

--- server/globals/enums/transactionCategoryEnum.js
+++ server/globals/enums/transactionCategoryEnum.js
@@ -1,7 +1,7 @@
 export const transactionCategoryEnum = Object.freeze({
-    PLATFORM: 'Platform Fee',
-    PAYMENT_PROVIDER: 'Payment Provider Fee',
-    WALLET_PROVIDER: 'Wallet Provider Fee',
-    ACCOUNT: 'Account to Account transaction',
-    CURRENCY_CONVERSION: 'Currency Conversion',
-  });
+  PLATFORM: 'Platform Fee',
+  PAYMENT_PROVIDER: 'Payment Provider Fee',
+  WALLET_PROVIDER: 'Wallet Provider Fee',
+  ACCOUNT: 'Account to Account transaction',
+  CURRENCY_CONVERSION: 'Currency Conversion',
+});
