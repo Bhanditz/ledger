6f94622e92d2e52d8899ca57ad814913b3eaabde
marcogbarcellos <marcogbarcellos@gmail.com>
2018-10-17 22:48:47 -0400

    feat(scripts): add forex and regular transactions cases

--- scripts/old-production-migration/transactionsTableMigration.js
+++ scripts/old-production-migration/transactionsTableMigration.js
@@ -12,32 +12,41 @@ const client = new Client({
 client.connect();
 // select t."FromCollectiveId" as "FromAccountId", t."FromCollectiveId" || '_' || t."PaymentMethodId" as "FromWalletId", t."CollectiveId" as "ToAccountId", t."CollectiveId" || '_Wallet' as "ToWalletId", t.amount, t.currency, t."amountInHostCurrency" as "destinationAmount", t."hostCurrency" as "destinationCurrency", t."hostFeeInHostCurrency" as "walletProviderFee", t."HostCollectiveId" as "WalletProviderAccountId", t."HostCollectiveId" || '_' || t."PaymentMethodId" as "WalletProviderWalletId", t."platformFeeInHostCurrency" as "platformFee", t."paymentProcessorFeeInHostCurrency" as "paymentProviderFee", p.service as "PaymentProviderAccountId", p.service as "PaymentProviderWalletId", t."hostCurrencyFxRate" as fxRate, t.id as "LegacyTransactionId" from "Transactions" t left join "PaymentMethods" p on t."PaymentMethodId"=p.id WHERE "TransactionGroup"='9d7ad496-55c3-4934-8472-780c152ade88' and t.type='CREDIT';
 // client.query('select t."FromCollectiveId" as "FromAccountId", t."FromCollectiveId" || \'_\' || t."PaymentMethodId" as "FromWalletId", t."CollectiveId" as "ToAccountId", t."CollectiveId" || \'_Wallet\' as "ToWalletId", t.amount, t.currency, t."amountInHostCurrency" as "destinationAmount", t."hostCurrency" as "destinationCurrency", t."hostFeeInHostCurrency" as "walletProviderFee", t."HostCollectiveId" as "WalletProviderAccountId", t."HostCollectiveId" || \'_\' || t."PaymentMethodId" as "WalletProviderWalletId", t."platformFeeInHostCurrency" as "platformFee", t."paymentProcessorFeeInHostCurrency" as "paymentProviderFee", p.service as "PaymentProviderAccountId", p.service as "PaymentProviderWalletId", t."hostCurrencyFxRate" as fxRate, t.id as "LegacyTransactionId" from "Transactions" t left join "PaymentMethods" p on t."PaymentMethodId"=p.id WHERE "TransactionGroup"=\'9d7ad496-55c3-4934-8472-780c152ade88\' and t.type=\'CREDIT\';', (err, res) => {
-  client.query('select t.*, p.service as "pmService", p.type as "pmType", c.name as "collectiveName", f.name as "fromCollectiveName", h.name as "hostName"'+
+
+// REGULAR Tx  -> "TransactionGroup"=\'e15f226d-1884-454c-a66c-3e2acc590ccc\'
+client.query('select t.*, p.service as "pmService", p.type as "pmType", c.name as "collectiveName", f.name as "fromCollectiveName", h.name as "hostName"'+
             ' from "Transactions" t left join "PaymentMethods" p on t."PaymentMethodId"=p.id' +
             ' left join "Collectives" c on t."CollectiveId"=c.id' +
             ' left join "Collectives" f on t."FromCollectiveId"=f.id' +
             ' left join "Collectives" h on t."HostCollectiveId"=h.id' +
-            ' WHERE "TransactionGroup"=\'9d7ad496-55c3-4934-8472-780c152ade88\' and t.type=\'CREDIT\';', (err, res) => {
+            ' WHERE "TransactionGroup"=\'e15f226d-1884-454c-a66c-3e2acc590ccc\' and t.type=\'CREDIT\';', (err, res) => {
+// FOREX Tx -> "TransactionGroup"=\'9d7ad496-55c3-4934-8472-780c152ade88\'
+// client.query('select t.*, p.service as "pmService", p.type as "pmType", c.name as "collectiveName", f.name as "fromCollectiveName", h.name as "hostName"'+
+//             ' from "Transactions" t left join "PaymentMethods" p on t."PaymentMethodId"=p.id' +
+//             ' left join "Collectives" c on t."CollectiveId"=c.id' +
+//             ' left join "Collectives" f on t."FromCollectiveId"=f.id' +
+//             ' left join "Collectives" h on t."HostCollectiveId"=h.id' +
+//             ' WHERE "TransactionGroup"=\'9d7ad496-55c3-4934-8472-780c152ade88\' and t.type=\'CREDIT\';', (err, res) => {
   if (err) console.error(err, res);
   if (!res || !res.rows || res.rows.length <= 0) console.error('Now records were found');
   console.log(`Raw Txs: ${JSON.stringify(res.rows, null,2)}`);
   const formattedLedgerTransaction = res.rows.map(transaction => {
     return {
       FromAccountId: transaction.FromCollectiveId, // `${transaction.FromCollectiveId}(${transaction.fromCollectiveName})`,
-      FromWalletId: transaction.PaymentMethodId,
+      FromWalletName: transaction.PaymentMethodId,
       ToAccountId:  transaction.CollectiveId, // `${transaction.CollectiveId}(${transaction.collectiveName})`,
-      ToWalletId: `${transaction.CollectiveId}_${transaction.HostCollectiveId}`, // Create a payment method
+      ToWalletName: `${transaction.CollectiveId}_${transaction.HostCollectiveId}`, // Create a payment method
       amount: transaction.amountInHostCurrency,
       currency: transaction.hostCurrency,
       destinationAmount: transaction.amount, // ONLY for FOREX transactions(currency != hostCurrency)
       destinationCurrency: transaction.currency, // ONLY for FOREX transactions(currency != hostCurrency)
       walletProviderFee: Math.round(-1 * transaction.hostFeeInHostCurrency/transaction.hostCurrencyFxRate),
       WalletProviderAccountId:  transaction.HostCollectiveId, // `${transaction.HostCollectiveId}(${transaction.hostName})`,
-      WalletProviderWalletId: transaction.HostCollectiveId,
+      WalletProviderWalletName: transaction.HostCollectiveId,
       platformFee: Math.round(-1 * transaction.platformFeeInHostCurrency/transaction.hostCurrencyFxRate),
       paymentProviderFee: Math.round(-1 * transaction.paymentProcessorFeeInHostCurrency/transaction.hostCurrencyFxRate),
       PaymentProviderAccountId: transaction.pmService, // PaymentMethod.service
-      PaymentProviderWalletId: transaction.pmType, // PaymentMethod.type
+      PaymentProviderWalletName: transaction.pmType, // PaymentMethod.type
       LegacyTransactionId: transaction.id,
     };
   })[0];
