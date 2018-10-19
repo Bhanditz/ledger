cf4a628b7ab816ef36227488be98d726adc242dd
marcogbarcellos <marcogbarcellos@gmail.com>
2018-10-19 14:09:06 -0400

    fix(routes/validators): fixing validation requirements on transaction validator

--- server/routes/validators/transactionValidator.js
+++ server/routes/validators/transactionValidator.js
@@ -1,4 +1,4 @@
-import { check } from 'express-validator/check';
+import { check, oneOf } from 'express-validator/check';
 import AbstractValidator from './abstractValidator';
 
 export default class TransactionValidator extends AbstractValidator {
@@ -9,21 +9,53 @@ export default class TransactionValidator extends AbstractValidator {
   post() {
     return [
       check('amount').isNumeric(),
-      check('destinationAmount').isNumeric(),
       check('currency').isAlphanumeric().isLength({ min: 3, max: 4 }),
-      check('destinationCurrency').isAlphanumeric().isLength({ min: 3, max: 4 }),
-      check('FromAccountId').isAlphanumeric(),
-      check('FromWalletName').isAlphanumeric(),
-      check('ToAccountId').isAlphanumeric(),
-      check('ToWalletName').isAlphanumeric(),
-      check('platformFee').isNumeric(), // Will have a default Wallet Id
-      check('paymentProviderFee').isNumeric(),
-      check('PaymentProviderAccountId').isAlphanumeric(),
-      check('PaymentProviderWalletName').isAlphanumeric(),
-      check('WalletProviderAccountId').isAlphanumeric(),
-      check('WalletProviderWalletName').isAlphanumeric(),
-      check('walletProviderFee').isNumeric(),
-      check('senderPayFees').isBoolean(), // default to false as receiver will pay fees by default
+      oneOf([
+        check('FromAccountId').isString(),
+        check('FromAccountId').isNumeric(),
+        check('FromAccountId').isAlphanumeric(),
+      ]),
+      oneOf([
+        check('FromWalletName').isString(),
+        check('FromWalletName').isNumeric(),
+        check('FromWalletName').isAlphanumeric(),
+      ]),
+      oneOf([
+        check('ToAccountId').isString(),
+        check('ToAccountId').isNumeric(),
+        check('ToAccountId').isAlphanumeric(),
+      ]),
+      oneOf([
+        check('ToWalletName').isString(),
+        check('ToWalletName').isNumeric(),
+        check('ToWalletName').isAlphanumeric(),
+      ]),
+      check('destinationAmount').optional().isNumeric(),
+      check('destinationCurrency').optional().isAlphanumeric().isLength({ min: 3, max: 4 }),
+      check('platformFee').optional().isNumeric(), // Will have a default Wallet Id
+      check('paymentProviderFee').optional().isNumeric(),
+      oneOf([
+        check('PaymentProviderAccountId').optional().isString(),
+        check('PaymentProviderAccountId').optional().isNumeric(),
+        check('PaymentProviderAccountId').optional().isAlphanumeric(),
+      ]),
+      oneOf([
+        check('PaymentProviderWalletName').optional().isString(),
+        check('PaymentProviderWalletName').optional().isNumeric(),
+        check('PaymentProviderWalletName').optional().isAlphanumeric(),
+      ]),
+      oneOf([
+        check('WalletProviderAccountId').optional().isString(),
+        check('WalletProviderAccountId').optional().isNumeric(),
+        check('WalletProviderAccountId').optional().isAlphanumeric(),
+      ]),
+      oneOf([
+        check('WalletProviderWalletName').optional().isString(),
+        check('WalletProviderWalletName').optional().isNumeric(),
+        check('WalletProviderWalletName').optional().isAlphanumeric(),
+      ]),
+      check('walletProviderFee').optional().isNumeric(),
+      check('senderPayFees').optional().isBoolean(), // default to false as receiver will pay fees by default
     ];
   }
 }
