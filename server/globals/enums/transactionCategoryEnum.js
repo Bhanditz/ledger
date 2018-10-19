09e25223b65ceca91e29f96352f2dbbf71d1b10a
marcogbarcellos <marcogbarcellos@gmail.com>
2018-10-18 20:27:51 -0400

    fix(enums): remove constants file and change transaction Account to Account Category name

--- server/globals/enums/transactionCategoryEnum.js
+++ server/globals/enums/transactionCategoryEnum.js
@@ -2,7 +2,7 @@ const transactionCategoryEnum = Object.freeze({
   PLATFORM: 'Platform Fee',
   PAYMENT_PROVIDER: 'Payment Provider Fee',
   WALLET_PROVIDER: 'Wallet Provider Fee',
-  ACCOUNT: 'Account to Account transaction',
+  ACCOUNT: 'Account to Account',
   CURRENCY_CONVERSION: 'Currency Conversion',
 });
 export default transactionCategoryEnum;
