bc3efe761d264a5de770d72f4ec35885f4e2bf01
marcogbarcellos <marcogbarcellos@gmail.com>
2018-10-18 20:23:42 -0400

    feat(scripts): adding stateful migration job

---
+++ scripts/old-production-migration/statefulMigration.js
@@ -0,0 +1,92 @@
+import PgAsync from 'pg-async';
+import TransactionService from '../../server/services/transactionService';
+import Database from '../../server/models';
+import Transaction from '../../server/models/Transaction';
+
+/*
+Backward migration, starts inserting by looking at the max id of the current prod's transaction table
+and then insert on the ledger(keeping state through the ledger's transaction field "LegacyTransactionId")
+Use Cases that migration is NOT working at the moment:
+  1. PaymentMethodId is null
+  2. PaymentMethods table has service defined but does NOT have type defined
+  3. FromCollectiveId is null(LegacyId 100784)
+  4. HostCollectiveId is null and/or hostCurrency is null(LegacyId 99531)
+*/
+export class StatefulMigration {
+
+  constructor() {
+    this.ledgerDatabase = new Database();
+  }
+
+  async getLatestLegacyIdFromLedger() {
+    const min = await Transaction.min('LegacyTransactionId');
+    return min || Number.MAX_SAFE_INTEGER;
+  }
+
+  async migrateSingleTransaction() {
+    const currentProdDbClient = new PgAsync({
+      user: 'apple',
+      host: 'localhost',
+      database: 'opencollective_prod_snapshot',
+      port: 5432,
+    });
+    const legacyId = await this.getLatestLegacyIdFromLedger();
+    console.log(`legacyId: ${legacyId}`);
+    const res = await currentProdDbClient.query(
+      'select t.*, p.service as "pmService", p.type as "pmType"'+
+      ' from "Transactions" t left join "PaymentMethods" p on t."PaymentMethodId"=p.id' +
+      ` WHERE t.id<${legacyId} and t.type=\'CREDIT\' and t."PaymentMethodId" is not null` +
+      ' and t."FromCollectiveId" is not null and t."HostCollectiveId" is not null ' +
+      ' and p.service is not null and p.type is not null'+
+      ' order by t.id desc limit 1;'
+    );
+    if (!res || !res.rows || res.rows.length <= 0)
+      console.error('Now records were found');
+
+    console.log(`Raw Txs: ${JSON.stringify(res.rows, null,2)}`);
+    const formattedLedgerTransaction = res.rows.map(transaction => {
+      return {
+        FromAccountId: transaction.FromCollectiveId, // `${transaction.FromCollectiveId}(${transaction.fromCollectiveName})`,
+        FromWalletName: transaction.PaymentMethodId,
+        ToAccountId:  transaction.CollectiveId, // `${transaction.CollectiveId}(${transaction.collectiveName})`,
+        ToWalletName: `${transaction.CollectiveId}_${transaction.HostCollectiveId}`, // Create a payment method
+        amount: transaction.amountInHostCurrency,
+        currency: transaction.hostCurrency,
+        destinationAmount: transaction.amount, // ONLY for FOREX transactions(currency != hostCurrency)
+        destinationCurrency: transaction.currency, // ONLY for FOREX transactions(currency != hostCurrency)
+        walletProviderFee: Math.round(-1 * transaction.hostFeeInHostCurrency/transaction.hostCurrencyFxRate),
+        WalletProviderAccountId:  transaction.HostCollectiveId, // `${transaction.HostCollectiveId}(${transaction.hostName})`,
+        WalletProviderWalletName: transaction.HostCollectiveId,
+        platformFee: Math.round(-1 * transaction.platformFeeInHostCurrency/transaction.hostCurrencyFxRate),
+        paymentProviderFee: Math.round(-1 * transaction.paymentProcessorFeeInHostCurrency/transaction.hostCurrencyFxRate),
+        PaymentProviderAccountId: transaction.pmService, // PaymentMethod.service
+        PaymentProviderWalletName: transaction.pmType, // PaymentMethod.type
+        LegacyTransactionId: transaction.id,
+      };
+    })[0];
+    console.log(`formattedLedgerTransaction: ${JSON.stringify(formattedLedgerTransaction, null,2)}`);
+    const service = new TransactionService();
+    try {
+      const res = await service.insert(formattedLedgerTransaction);
+      return res;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async run() {
+    try {
+      console.log('migrating one more row...');
+      const migratedTransaction = await this.migrateSingleTransaction();
+      console.log(`MIGRATED tx: ${JSON.stringify(migratedTransaction, null,2)}`);
+      setTimeout(this.run.bind(this), 500);
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
+    }
+  }
+
+}
+
+const migration = new StatefulMigration();
+migration.run();
--- scripts/old-production-migration/transactionsTableMigration.js
+++ scripts/old-production-migration/transactionsTableMigration.js
@@ -14,19 +14,19 @@ client.connect();
 // client.query('select t."FromCollectiveId" as "FromAccountId", t."FromCollectiveId" || \'_\' || t."PaymentMethodId" as "FromWalletId", t."CollectiveId" as "ToAccountId", t."CollectiveId" || \'_Wallet\' as "ToWalletId", t.amount, t.currency, t."amountInHostCurrency" as "destinationAmount", t."hostCurrency" as "destinationCurrency", t."hostFeeInHostCurrency" as "walletProviderFee", t."HostCollectiveId" as "WalletProviderAccountId", t."HostCollectiveId" || \'_\' || t."PaymentMethodId" as "WalletProviderWalletId", t."platformFeeInHostCurrency" as "platformFee", t."paymentProcessorFeeInHostCurrency" as "paymentProviderFee", p.service as "PaymentProviderAccountId", p.service as "PaymentProviderWalletId", t."hostCurrencyFxRate" as fxRate, t.id as "LegacyTransactionId" from "Transactions" t left join "PaymentMethods" p on t."PaymentMethodId"=p.id WHERE "TransactionGroup"=\'9d7ad496-55c3-4934-8472-780c152ade88\' and t.type=\'CREDIT\';', (err, res) => {
 
 // REGULAR Tx  -> "TransactionGroup"=\'e15f226d-1884-454c-a66c-3e2acc590ccc\'
-client.query('select t.*, p.service as "pmService", p.type as "pmType", c.name as "collectiveName", f.name as "fromCollectiveName", h.name as "hostName"'+
-            ' from "Transactions" t left join "PaymentMethods" p on t."PaymentMethodId"=p.id' +
-            ' left join "Collectives" c on t."CollectiveId"=c.id' +
-            ' left join "Collectives" f on t."FromCollectiveId"=f.id' +
-            ' left join "Collectives" h on t."HostCollectiveId"=h.id' +
-            ' WHERE "TransactionGroup"=\'e15f226d-1884-454c-a66c-3e2acc590ccc\' and t.type=\'CREDIT\';', (err, res) => {
-// FOREX Tx -> "TransactionGroup"=\'9d7ad496-55c3-4934-8472-780c152ade88\'
 // client.query('select t.*, p.service as "pmService", p.type as "pmType", c.name as "collectiveName", f.name as "fromCollectiveName", h.name as "hostName"'+
 //             ' from "Transactions" t left join "PaymentMethods" p on t."PaymentMethodId"=p.id' +
 //             ' left join "Collectives" c on t."CollectiveId"=c.id' +
 //             ' left join "Collectives" f on t."FromCollectiveId"=f.id' +
 //             ' left join "Collectives" h on t."HostCollectiveId"=h.id' +
-//             ' WHERE "TransactionGroup"=\'9d7ad496-55c3-4934-8472-780c152ade88\' and t.type=\'CREDIT\';', (err, res) => {
+//             ' WHERE "TransactionGroup"=\'e15f226d-1884-454c-a66c-3e2acc590ccc\' and t.type=\'CREDIT\';', (err, res) => {
+// FOREX Tx -> "TransactionGroup"=\'9d7ad496-55c3-4934-8472-780c152ade88\'
+client.query('select t.*, p.service as "pmService", p.type as "pmType", c.name as "collectiveName", f.name as "fromCollectiveName", h.name as "hostName"'+
+            ' from "Transactions" t left join "PaymentMethods" p on t."PaymentMethodId"=p.id' +
+            ' left join "Collectives" c on t."CollectiveId"=c.id' +
+            ' left join "Collectives" f on t."FromCollectiveId"=f.id' +
+            ' left join "Collectives" h on t."HostCollectiveId"=h.id' +
+            ' WHERE "TransactionGroup"=\'9d7ad496-55c3-4934-8472-780c152ade88\' and t.type=\'CREDIT\';', (err, res) => {
   if (err) console.error(err, res);
   if (!res || !res.rows || res.rows.length <= 0) console.error('Now records were found');
   console.log(`Raw Txs: ${JSON.stringify(res.rows, null,2)}`);
