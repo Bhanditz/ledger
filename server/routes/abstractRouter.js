8ea8391059786d9de6482e998c3142984fced148
marcogbarcellos <marcogbarcellos@gmail.com>
2018-10-26 16:54:38 -0400

    fix(routes): disabling temporarily validator due to json issue

--- server/routes/abstractRouter.js
+++ server/routes/abstractRouter.js
@@ -30,7 +30,7 @@ export default class AbstractRouter {
   }
 
   post() {
-    this.app.post(this.path, this.validator ? this.validator.post() : [], (req, res) => {
+    this.app.post(this.path,/* this.validator ? this.validator.post() : [],*/ (req, res) => {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
         return res.status(422).json({ errors: errors.array() });
