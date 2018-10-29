60c609d0d643f91a0b08f433cda2b5f906e6ab0a
marcogbarcellos <marcogbarcellos@gmail.com>
2018-10-29 13:43:57 -0400

    feat(config): add amqp queue config

--- config/config.js
+++ config/config.js
@@ -1,11 +1,14 @@
 export default {
-    port: process.env.PORT || 3060,
-    host: process.env.HOST || '0.0.0.0',
     database: {
         database: process.env.DB_NAME || 'opencollective_ledger_dvl',
         username: process.env.DB_USER || 'opencollective',
         password: process.env.DB_PASSWORD || '',
         host: process.env.DB_HOST || '127.0.0.1',
         dialect: process.env.DB_DIALECT || 'postgres',
+    },
+    host: process.env.HOST || '0.0.0.0',
+    port: process.env.PORT || 3060,
+    queue: {
+        url: process.env.QUEUE_URL ? `amqp://${process.env.QUEUE_URL}` : 'amqp://localhost',
     },
 };
