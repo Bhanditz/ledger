3c027af9fe9c797e53173bb6a37243b8fbe9fed4
marcogbarcellos <marcogbarcellos@gmail.com>
2018-11-08 17:58:32 -0500

    fix(queue/transactions): stop resending data to queue in error handling

--- server/queue/transactionsWorker.js
+++ server/queue/transactionsWorker.js
@@ -10,6 +10,11 @@ export default class TransactionsWorker {
     this.transactionService = new TransactionService();
   }
 
+  async sendToQueue(channel, transaction) {
+    await channel.assertQueue(config.queue.transactionQueue, { exclusive: false });
+    channel.sendToQueue(config.queue.transactionQueue, Buffer.from(JSON.stringify(transaction), 'utf8'));
+  }
+
   /** Opens Queue channel to wait for incoming transactions to be consumed
   * @return {void}
   */
@@ -19,14 +24,21 @@ export default class TransactionsWorker {
     const q = await channel.assertQueue(config.queue.transactionQueue, { exclusive: false });
     this.logger.info('Transactions Queue Worker has started.');
     channel.consume(q.queue, async (msg) => {
+      let incomingTransaction = null;
       try {
         this.logger.info(` [x] ${msg.fields.routingKey}:'${msg.content.toString()}'`);
-        const incomingTransaction = JSON.parse(msg.content);
+        incomingTransaction = JSON.parse(msg.content);
         const parsedTransactions = await this.transactionService.parseAndInsertTransaction(incomingTransaction);
         this.logger.info(`Transaction Parsed and inserted successfully,
           list of generated transactions: ${JSON.stringify(parsedTransactions, null, 2)}`);
       } catch (error) {
         this.logger.error(error);
+        // if there is an error with a transaction, resend to queue
+        // if (incomingTransaction) {
+        //   this.logger.warn('Resending transaction to queue...');
+        //   this.sendToQueue(channel, incomingTransaction);
+        // }
+
       }
     }, { noAck: true });
   }
