4cebbde6ddfd27fdfe59182e87ccc83af1705fde
marcogbarcellos <marcogbarcellos@gmail.com>
2018-11-19 18:58:03 -0500

    refactor: removing unnecessary info

--- server/strategies/transactionForexStrategy.js
+++ server/strategies/transactionForexStrategy.js
@@ -62,24 +62,12 @@ export default class TransactionForexStrategy extends AbstractTransactionForexSt
       return transaction;
     });
 
-    // after calculating all fees, set the amount of transaction as the destinationAmount
-    // this.incomingTransaction.amount = this.incomingTransaction.destinationAmount;
-
     // if senderPayFees, he will discount the fees from the total amount to send the net amount to the receiver
     // otherwise the sender will send the full amount and the receiver will pay the fees
     if (this.incomingTransaction.senderPayFees) {
       // calculating netAmount of the regular transaction
       this.incomingTransaction.amount = this.getTransactionNetAmount(paymentProviderFeeTransactions, platformFeeTransactions, providerFeeTransactions);
     }
-    // modifying incomingTransaction so we can use the new information to generate the forex initial transaction
-    // this.incomingTransaction.FromWalletId = this.incomingTransaction.fromWalletDestinationCurrency.id;
-    // this.incomingTransaction.currency = this.incomingTransaction.destinationCurrency;
-    // setting initial destination currency transaction after having a net amount(total amount - fees)
-    // const initialDestinationCurrencyTransactions = this.transactionLib.getDoubleEntryArray(this.incomingTransaction)
-    // .map(transaction => {
-    //   transaction.category = transactionCategoryEnum.ACCOUNT;
-    //   return transaction;
-    // });
     // generate all Double Entry transactions
     return this.getAllTransactionsWithFee([...initialDestinationCurrencyTransactions, ...conversionTransactions],
       paymentProviderFeeTransactions, platformFeeTransactions, providerFeeTransactions);
