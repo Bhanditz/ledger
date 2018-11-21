042e5b9d4288e6fe4ed2373a2e3a93eb3f2ab9c9
marcogbarcellos <marcogbarcellos@gmail.com>
2018-11-21 07:54:54 -0500

    feat: add port and ssl mode to database config

--- config/config.js
+++ config/config.js
@@ -2,9 +2,11 @@ export default {
     database: {
         database: process.env.DB_NAME || 'opencollective_prod_snapshot',
         username: process.env.DB_USER || 'opencollective',
+        port: process.env.DB_PORT || 5432,
         password: process.env.DB_PASSWORD || '',
         host: process.env.DB_HOST || '127.0.0.1',
         dialect: process.env.DB_DIALECT || 'postgres',
+        sslMode: process.env.DB_SSL_MODE || false,
     },
     host: process.env.HOST || '0.0.0.0',
     port: process.env.PORT || 3060,
