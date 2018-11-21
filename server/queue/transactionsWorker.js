bea33c95c21aa943e881b950e4f97bd5f93d7808
marcogbarcellos <marcogbarcellos@gmail.com>
2018-11-21 18:20:19 -0500

    fix: queue consumer improvements regarding open connections

--- server/queue/transactionsWorker.js
+++ server/queue/transactionsWorker.js
@@ -2,7 +2,6 @@ import amqp from 'amqplib';
 import config from '../../config/config';
 import Logger from '../globals/logger';
 import TransactionService from '../services/transactionService';
-import Database from '../models';
 
 export default class TransactionsWorker {
 
@@ -11,21 +10,25 @@ export default class TransactionsWorker {
     this.transactionService = new TransactionService();
   }
 
-  async sendToQueue(channel, transaction) {
-    await channel.assertQueue(config.queue.transactionQueue, { exclusive: false });
-    channel.sendToQueue(config.queue.transactionQueue, Buffer.from(JSON.stringify(transaction), 'utf8'));
+  /** Opens and returns Queue channel to wait for incoming
+   * transactions to be consumed
+  * @return {void}
+  */
+  async getAmqpChannel() {
+    if (!this.amqpChannel) {
+      console.log('Initializing queue...');
+      this.amqpConnection = await amqp.connect(config.queue.url);
+      this.amqpChannel = await this.amqpConnection.createChannel();
+    }
+    return this.amqpChannel;
   }
 
   /** Opens Queue channel to wait for incoming transactions to be consumed
   * @return {void}
   */
   async consume() {
-    // getting db pools and connections
-    const ledgerDatabase = new Database();
-    const pool = ledgerDatabase.sequelize.connectionManager.pool;
     // connecting to queue server
-    const conn = await amqp.connect(config.queue.url);
-    const channel = await conn.createChannel();
+    const channel = await this.getAmqpChannel();
     const q = await channel.assertQueue(config.queue.transactionQueue, { exclusive: false });
     this.logger.info('Transactions Queue Worker has started.');
     channel.consume(q.queue, async (msg) => {
@@ -35,15 +38,6 @@ export default class TransactionsWorker {
         this.logger.info('Transaction Parsed and inserted successfully');
       } catch (error) {
         this.logger.error(error);
-        // if there is an error with a transaction, resend to queue
-        // if (incomingTransaction) {
-        //   this.logger.warn('Resending transaction to queue...');
-        //   this.sendToQueue(channel, incomingTransaction);
-        // }
-      } finally {
-        // releasing pool size
-        pool.drain();
-        pool.clear();
       }
     }, { noAck: true });
   }
