b4162af575015efd49a75695cfc68558ad5a4900
marcogbarcellos <marcogbarcellos@gmail.com>
2018-11-23 09:54:57 -0500

    feat: adding lru cache to get and getOne mtransactions services method

--- server/services/abstractCrudService.js
+++ server/services/abstractCrudService.js
@@ -1,5 +1,6 @@
 import Database from '../models';
 import Logger from '../globals/logger';
+import LRU from 'lru-cache';
 
 export default class AbstractCrudService {
 
@@ -7,14 +8,36 @@ export default class AbstractCrudService {
     this.database = new Database();
     this.model = model;
     this.logger = new Logger();
+    this.cache = new LRU({
+      max: 10000,
+      maxAge: 1000 * 60 * 10, // we keep it max 10 min
+    });
   }
 
-  get(query = {}) {
-    return this.model.findAll({ where: query });
+  async get(query = {}) {
+    const key = `${this.model.name}_${JSON.stringify(query)}_all`;
+    const cacheItem = this.cache.get(key);
+    if (cacheItem) {
+      return cacheItem;
+    }
+    const result = await this.model.findAll({ where: query });
+    this.cache.set(key, result);
+    return result;
   }
 
   getOne(query = {}) {
-    return this.model.findOne({ where: query });
+    const key = `${this.model.name}_${JSON.stringify(query)}_one`;
+    const cacheItem = this.cache.get(key);
+    if (cacheItem) {
+      return cacheItem;
+    }
+    return this.model.findOne({ where: query })
+    .then(result => {
+      this.cache.set(key, result);
+      return result;
+    }).catch(error => {
+      throw error;
+    });
   }
 
   insert(data) {
