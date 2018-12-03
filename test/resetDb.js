70fe05d2072b9a5bb5380fb8cfeac8bc5f334c17
marcogbarcellos <marcogbarcellos@gmail.com>
2018-12-03 17:05:43 -0500

    fix(test): remove unexisting model

--- test/resetDb.js
+++ test/resetDb.js
@@ -1,12 +1,11 @@
 import Database from '../server/models';
+import config from '../config/config';
 
 export default class ResetDb {
   static async run() {
     const db = new Database();
     for (const model of Object.keys(db.models)) {
-      if (model.toString() === 'Account') {
-        await db.sequelize.query('DELETE FROM "Accounts";');
-      } else if (model.toString() === 'Wallet') {
+      if (model.toString() === 'Wallet') {
         await db.sequelize.query('DELETE FROM "Wallets";');
       } else {
         await db.models[model].sync({ force: true });
