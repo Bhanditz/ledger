c7e7d7cd6ade3558979c47f910b9d479e61196a6
marcogbarcellos <marcogbarcellos@gmail.com>
2018-12-06 07:51:04 -0500

    refactor(strategies/transactionForex): remove method to split strategy into 2

--- server/strategies/transactionForexStrategy.js
+++ server/strategies/transactionForexStrategy.js
@@ -1,5 +1,4 @@
 import AbstractTransactionForexStrategy from './abstractTransactionForexStrategy';
-import ForexConversionTransactions from '../lib/forexConversionTransactions';
 import transactionCategoryEnum from '../globals/enums/transactionCategoryEnum';
 import ForexToAccountConvertTransactions from '../lib/forexToAccountConvertTransactions';
 
@@ -9,38 +8,7 @@ export default class TransactionForexStrategy extends AbstractTransactionForexSt
     super(incomingTransaction);
   }
 
-  async getTransactionsWithFromAccountConvertingCurrency() {
-    await this.findOrCreateWallets(true);
-    const [paymentProviderFeeTransactions, platformFeeTransactions, providerFeeTransactions] = await this.getFeeTransactions();
-    const conversionTransactionsManager = new ForexConversionTransactions(this.incomingTransaction);
-    const conversionTransactions = conversionTransactionsManager.getForexDoubleEntryTransactions()
-    .map(transaction => {
-      transaction.category = transactionCategoryEnum.CURRENCY_CONVERSION;
-      return transaction;
-    });
-    // after calculating all fees, set the amount of transaction as the destinationAmount
-    this.incomingTransaction.amount = this.incomingTransaction.destinationAmount;
-    // if senderPayFees, he will discount the fees from the total amount to send the net amount to the receiver
-    // otherwise the sender will send the full amount and the receiver will pay the fees
-    if (this.incomingTransaction.senderPayFees) {
-      // calculating netAmount of the regular transaction
-      this.incomingTransaction.amount = this.getTransactionNetAmount(paymentProviderFeeTransactions, platformFeeTransactions, providerFeeTransactions);
-    }
-    // modifying incomingTransaction so we can use the new information to generate the forex initial transaction
-    this.incomingTransaction.FromWalletId = this.incomingTransaction.fromWalletDestinationCurrency.id;
-    this.incomingTransaction.currency = this.incomingTransaction.destinationCurrency;
-    // setting initial destination currency transaction after having a net amount(total amount - fees)
-    const initialDestinationCurrencyTransactions = this.transactionLib.getDoubleEntryArray(this.incomingTransaction)
-    .map(transaction => {
-      transaction.category = transactionCategoryEnum.ACCOUNT;
-      return transaction;
-    });
-    // generate all Double Entry transactions
-    return this.getAllTransactionsWithFee([...conversionTransactions, ...initialDestinationCurrencyTransactions],
-      paymentProviderFeeTransactions, platformFeeTransactions, providerFeeTransactions);
-  }
-
-  async getTransactions() { // getTransactionsWithToAccountConvertingCurrency
+  async getTransactions() {
     await this.findOrCreateWallets(false);
     // setting account to account transactions
     const transactionWithToWalletInSourceCurrency = {
@@ -61,7 +29,6 @@ export default class TransactionForexStrategy extends AbstractTransactionForexSt
       transaction.category = transactionCategoryEnum.CURRENCY_CONVERSION;
       return transaction;
     });
-
     // if senderPayFees, he will discount the fees from the total amount to send the net amount to the receiver
     // otherwise the sender will send the full amount and the receiver will pay the fees
     if (this.incomingTransaction.senderPayFees) {
